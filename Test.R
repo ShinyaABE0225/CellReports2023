a1078aaf200b49ad12b534fe204604a98d05e91e
ShinyaABE0225 <141819588+ShinyaABE0225@users.noreply.github.com>
2023-08-16 17:38:20 +0900

    Delete Test.R

--- Test.R
+++
@@ -1,7 +0,0 @@
-#Test
-distanceNK <- array(0, c(dim(NK)[1]))
-for (i in 1:dim(NK)[1]){
-  displacement <- NK1 - cbind(rep(NK[i,1],dim(NK1)[1]), rep(NK[i,2], dim(NK1)[1]))
-  displacement2 <- sort(sqrt(rowSums(displacement^2)))
-  distanceNK[i] <- displacement2[2]
-}
